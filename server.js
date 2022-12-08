373fce776fe5495f9985d14499981d98c5b65e8d
Stefan Iordache <stefaniordache472@gmail.com>
2022-12-08 15:55:15 +0200

    add form post

--- server.js
+++ server.js
@@ -7,10 +7,17 @@ const fileReader = require("./fileReader");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname))
 
 const port = 9000;
+
+
+let id = 0
+let order = []
+let date = {}
+let customer = {}
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(`${__dirname}/homepage.html`));
@@ -28,8 +35,36 @@ app.get("/api/pizza", async (req, res) => {
     res.send(data)
   });
 
-app.get("/pizza/list", async (req, res) => {
+
+app.get("/api/order", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/order.json`));
+});
+
+//JSON.stringify(test, null, 2)
+app.post("/order", async (req, res) => {
+  fs.writeFileSync(`${__dirname}/order.json`, "{\"orders\":"+ {} + "}");
+  res.sendFile(path.join(`${__dirname}/order.json`));
+}); 
+
+app.post("/pizza/list", async (req, res) => {
+  order = req.body
+  id++
+  console.log(order)
+});
+
+app.get("/pizza/list", (req, res) => {
     res.sendFile(path.join(`${__dirname}/pizzaList.html`));
-  });
+});
+
+app.get("/form", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/form.html`));
+});
+
+app.post("/form", (req, res) => {
+  customer.customer = req.body
+  let tempOrder = {"id" : id, "pizzas" : order, "customer" : customer}
+  console.log(tempOrder);
+  fs.writeFileSync(`${__dirname}/order.json`, "{\"orders\":" + JSON.stringify(tempOrder, null, 2) +"}")
+});
 
 app.listen(port, _ => console.log(`http://127.0.0.1:${port}`));
